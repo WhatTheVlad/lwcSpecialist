7424e0ac94763858d5c5f3a3d0dcd393a8163d2b
Mocanu Vlad <vlad.mocanu@focality.ro>
2021-05-06 18:23:56 +0300

    [Challenge #4] - added starter boatSearchForm component"

---
+++ force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
@@ -0,0 +1,13 @@
+/**
+ * Created by mocan on 06/05/2021.
+ */
+
+import { LightningElement } from "lwc";
+
+export default class BoatSearchForm extends LightningElement {
+
+  handleClick(event) {
+    console.log('clicked button');
+  }
+
+}
